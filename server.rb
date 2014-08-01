e0a7d189bde2a9d85fbe28f0774f227e1c45d572
Brittney Kistner <bkistner01@gmail.com>
2014-08-01 12:47:06 -0700

    Updates to the menu tabs and types_of_item lists for each menu

--- server.rb
+++ server.rb
@@ -23,28 +23,26 @@ get '/menu' do
   @title = "Menu"
   @menus = Restaurant::Menu.get_all
 
+
+#types_of_item_by_menu = {'menu1'=> [{'beverages'=> ['coke', 'coffee']},{'pancakes'=> ['1stack', '2stack']}], 'menu2'=> [{'beverages'=> ['coke', 'coffee']},{'entrees'=> ['hamburger', 'salad']}] }
   @types_of_item_by_menu = {}
   @menus.each do |menu|
-    types_of_item = []
+    types_of_items = []
+    foods_by_type_of_item = {}
     foods = menu.list_food
     foods.each do |food|
-      types_of_item.include?(food.type_of_item) ?  nil : types_of_item << food.type_of_item
-      types_of_item.compact!
+      if types_of_items.include?(food.type_of_item)
+        types_of_items << food.type_of_item
+      end
+      if foods_by_type_of_item.include?(food.type_of_item)
+        @foods_by_type_of_item[type_of_item] << food
+      else
+        @foods_by_type_of_item[type_of_item] = [food]
+      end
+      types_of_items
     end
-    @types_of_item_by_menu[menu] = types_of_item
+    @types_of_item_by_menu[menu] = types_of_items
   end
 
-  # @food_name_by_type_and_menu = {}
-  # @menu.each do |menu|
-  #   types_of_item = []
-  #   foods = menu.list_food
-  #   foods.each do |food|
-  #     types_of_item.include?(food.type_of_item) ?  nil : types_of_item << food.type_of_item
-  #     types_of_item.compact!
-  #   end
-  #   food_by_type = []
-
-  #first loop will iterate through the different menus
-  #second loop will put out the index, type of item, and food information
   erb :"/customer/customer_menu"
 end
